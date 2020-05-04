f16a145f577283a786596254dd99e43bb7acd029
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-05-04 09:46:33 +0100

    done with the score and result calculations

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ var recordText = document.getElementById("record-text");
 var questionCon = document.getElementById("question-con"); //this
 var questionNumber = document.getElementById("question-number"); //this
 var nextButton = document.getElementById("next");
+var myScore = [];
+var myResult = 0;
 
 var questionBank = [
     ["What is the arithmetic mean of the following numbers: 10, 20, 50, 40?", "20", "30", "40", "50", "None of the above"],
@@ -16,7 +18,10 @@ var questionBank = [
 ]
 
 var myQuestion = document.getElementById("question"); //this
+var paraOption = document.getElementsByClassName("option")
+var optionSelected = document.getElementsByClassName("options");
 var myOption = document.getElementsByClassName("option-value");
+
 
 var questionWord = {
     0: "zero",
@@ -39,7 +44,50 @@ function questionCounter(){
     // console.log(myCounter);
     return myCounter;
 }
-//END of function that records total number of question answered
+//(END of function that records total number of question answered
+
+
+// function that determines correct or wrong answer
+function correctAnswer(c){
+    if(myCounter == 0 && c.value == "c"){
+        // paraOption[2].style.backgroundColor = "Green";
+        myScore[myCounter] = 1;
+    } else if(myCounter == 1 && c.value == "b"){
+        // paraOption[1].style.backgroundColor = "Green";
+        myScore[myCounter] = 1;
+    } else if(myCounter == 2 && c.value == "a"){
+        // paraOption[0].style.backgroundColor = "Green";
+        myScore[myCounter] = 1;
+    } else if(myCounter == 3 && c.value == "d"){
+        // paraOption[3].style.backgroundColor = "Green";
+        myScore[myCounter] = 1;
+    } else if(myCounter == 4 && c.value == "d"){
+        // paraOption[3].style.backgroundColor = "Green";
+        myScore[myCounter] = 1;
+    } else{
+        // paraOption[i].style.backgroundColor = "Red";
+        myScore[myCounter] = 0;
+    }
+    console.log(myScore); 
+}
+//END of function that determines correct or wrong answer
+
+
+// Function that calculates the score
+function calResult(){
+    for(var i in myScore){
+        myResult += myScore[i];
+    }
+    return myResult;
+}
+// END of Function that calculates the score
+// if(myCounter == 0 && c.select && c.value == "c")
+
+function backgroundReset(){
+    for(var i of paraOption){
+        i.style.backgroundColor = "none";
+    }
+}
 
 
 // function for nexting question
@@ -49,7 +97,6 @@ function nextQuestion(){
     // calls the function that records total number of question answered
     questionCounter();
     // END of function that records total number of question answered
-
     if(myCounter <= 4){
         recordText.innerHTML = "You have answered " + questionWord[myCounter] + " " + (myCounter) + " question(s) out of five(5) questons";
         questionCon.style.display = "block";
@@ -62,16 +109,16 @@ function nextQuestion(){
         }
         // END of option-value
 
-
     } else if(myCounter == 5){
         recordText.innerHTML = "You have answered " + questionWord[myCounter] + " " + (myCounter) + " question(s) out of five(5) questons";
         questionCon.style.display = "none";
         nextButton.innerHTML = "Check Result";
     } else{
+        console.log(myResult);
         nextButton.innerHTML = "Test Completed";
         nextButton.style.backgroundColor = "red";
         nextButton.setAttribute("disabled", "disabled")
-        recordText.innerHTML = "Your score is .... ";
+        recordText.innerHTML = "Your score is .... " + calResult() + " / " + (myCounter-1);
     }
 }
 //END of function for nexting question
